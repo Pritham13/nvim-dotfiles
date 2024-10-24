8fa4a20c80f0eb7dba8950bb92453f0f873c263b
zero <prithamhsuvarna@gmail.com>
2024-10-24 23:30:48 +0530

    updating files

--- lua/vim-opt.lua
+++ lua/vim-opt.lua
@@ -8,4 +8,9 @@ vim.keymap.set('n', 'H', ':bprevious<CR>', { noremap = true, silent = true })  -
 -- Keybindings for navigating between windows
 vim.keymap.set('n', '<C-h>', '<C-w>h', { noremap = true, silent = true })  -- Ctrl+h to move to the left window
 vim.keymap.set('n', '<C-l>', '<C-w>l', { noremap = true, silent = true })  -- Ctrl+l to move to the right window
+-- Split window horizontally
+vim.api.nvim_set_keymap('n', '<leader>h', ':split<CR>', { noremap = true, silent = true })
+
+-- Split window vertically
+vim.api.nvim_set_keymap('n', '<leader>v', ':vsplit<CR>', { noremap = true, silent = true })
 
