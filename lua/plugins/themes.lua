ec6549e37e618c76de4ac078260743e13ba94693
zero <prithamhsuvarna@gmail.com>
2024-12-27 21:21:47 +0530

    minor changes

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -1,34 +1,97 @@
--- return{
---     "catppuccin/nvim",
---     name = "catppuccin",
---     priority = 1000 ,
---     config = function()
---         vim.cmd.colorscheme "catppuccin"
---     end
--- }
--- Lazy
--- return {
--- 	"olimorris/onedarkpro.nvim",
--- 	priority = 1000, -- Ensure it loads first
--- 	config = function()
--- 		vim.cmd.colorscheme "onedark"
--- 	end
--- }
-return {
-  {
-    "navarasu/onedark.nvim",
+------------------------------- catppuccin Theme ---------------------------
+return{
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000 ,
     config = function()
-      -- Set the colorscheme to OneDark
-      vim.cmd.colorscheme("onedark")
-
-      -- Optional: Additional configuration for OneDark
-      require('onedark').setup {
-        style = 'darker',              -- Choose your preferred style
-        transparent = false,           -- Set to true for a transparent background
-        term_colors = true,            -- Enable terminal colors
-        ending_tildes = false,         -- Show end-of-buffer tildes
-      }
-      require('onedark').load()        -- Load the configured theme
-    end,
-  },
+        vim.cmd.colorscheme "catppuccin"
+    end
 }
+------------------------------- One dark theme -----------------------------------------------------
+-- return {
+--   {
+--     "navarasu/onedark.nvim",
+--     config = function()
+--       -- Set the colorscheme to OneDark
+--       vim.cmd.colorscheme("onedark")
+--
+--       -- Optional: Additional configuration for OneDark
+--       require('onedark').setup {
+--         style = 'darker',              -- Choose your preferred style
+--         transparent = false,           -- Set to true for a transparent background
+--         term_colors = true,            -- Enable terminal colors
+--         ending_tildes = false,         -- Show end-of-buffer tildes
+--       }
+--       require('onedark').load()        -- Load the configured theme
+--     end,
+--   },
+-- }
+-------------------------------------kanagawa ------------------------------------------------------
+-- local strongHighlight = "#fa7af6"
+-- return {
+--   "rebelot/kanagawa.nvim",
+--   priority = 1000,
+--   config = function()
+--     -- Make background transparent. I like semi-transparent background in the terminal.
+--     vim.cmd([[
+--     augroup user_colors
+--       autocmd!
+--       autocmd ColorScheme * highlight Normal ctermbg=NONE guibg=NONE
+--     augroup END
+--   ]])
+--
+--     require("kanagawa").setup({
+--       undercurl = true, -- enable undercurls
+--       commentStyle = {
+--         italic = true,
+--       },
+--       colors = {
+--         theme = {
+--           all = {
+--             ui = {
+--               bg_gutter = "none"
+--             }
+--           }
+--         }
+--       },
+--       functionStyle = {},
+--       keywordStyle = {
+--         italic = true,
+--       },
+--       statementStyle = {},
+--       typeStyle = {},
+--       variablebuiltinStyle = {
+--         italic = true,
+--       },
+--       overrides = function(colors)
+--         local theme = colors.theme
+--         return {
+--           IncSearch = { fg = "black", bg = strongHighlight, underline = true, bold = true },
+--           Search = { fg = "black", bg = colors.palette.oniViolet },
+--           Substitute = { fg = "black", bg = colors.palette.strongHighlight },
+--           Pmenu = { fg = theme.ui.shade0, bg = colors.palette.sumiInk3 },
+--           PmenuSel = { fg = "NONE", bg = theme.ui.bg_p4 },
+--           PmenuSbar = { bg = theme.ui.bg_p2 },
+--           PmenuThumb = { bg = theme.ui.bg_p2 },
+--         }
+--       end,
+--       specialReturn = true,    -- special highlight for the return keyword
+--       specialException = true, -- special highlight for exception handling keywords
+--       transparent = false,     -- do not set background color
+--     })
+--
+--
+--     -- vim.api.nvim_set_hl(0, "@tag", { fg = colors.lightBlue })
+--     -- vim.api.nvim_set_hl(0, "@tag.delimiter", { fg = colors.lightBlue, })
+--     -- vim.api.nvim_set_hl(0, "@tag.attribute", { fg = colors.sakuraPink })
+--
+--     vim.cmd.hi("NonText guifg=bg")
+--     vim.cmd("colorscheme kanagawa")
+--
+--     -- Highlight active line
+--     vim.opt.cursorline = true
+--     vim.opt.cursorlineopt = 'number'
+--     vim.cmd.highlight('CursorLineNr guibg=#1a1a22')
+--     vim.cmd.highlight('SignColumnNr guibg=#1a1a22')
+--   end
+
--- lua/vim-opt.lua
+++ lua/vim-opt.lua
@@ -13,20 +13,14 @@ vim.keymap.set('n', '<C-h>', '<C-w>h', { noremap = true, silent = true }) -- Ctr
 vim.keymap.set('n', '<C-l>', '<C-w>l', { noremap = true, silent = true }) -- Ctrl+l to move to the right window
 -- Split window horizontally
 vim.api.nvim_set_keymap('n', '<leader>h', ':split<CR>', { noremap = true, silent = true })
-vim.keymap.set({'i'}, '<C-s>', '<C-o>:w<ENTER>')
-vim.keymap.set({'n'}, '<C-s>', ':w<ENTER>')
+vim.keymap.set({'i'}, '<C-s>', '<C-o>:wa<ENTER>')
+vim.keymap.set({'n'}, '<C-s>', ':wa<ENTER>')
 
 -- Split window vertically
 vim.api.nvim_set_keymap('n', '<leader>v', ':vsplit<CR>', { noremap = true, silent = true })
 vim.wo.relativenumber = true
 vim.api.nvim_set_keymap('n', '<Leader>bd', ':bd<CR>', { noremap = true, silent = true })
 
-vim.keymap.set('n', '<leader>iv', function()
-  -- Run iverilog on the current file and send output to the quickfix list
-  vim.cmd("cexpr system('iverilog -o testbench_out.out % &&  ./testbench_out')")
-  -- Open the quickfix list if there are any errors or warnings
-  vim.cmd("cwindow")
-end, { desc = "Run Iverilog Testbench and Show Errors" })
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a' 
 
